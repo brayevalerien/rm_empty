e72d307b073898bb6ef6282d12b749eff547494b
Vanonym <60972074+brayevalerien@users.noreply.github.com>
2023-02-23 16:48:47 +0100

    The program can now be run in terminal mode.

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # rm_empty
- A small utlity tool to remove all useless empty directories at a given location.
+ A small utility tool to remove all useless empty directories at a given location.
 
  ## How to use
-Make sure you have `tkinter` properly installed on you computer. Then you can run [rm_empty.py](./rm_empty.py) and a small UI will pop up.
-Either use the browse directories button to chose the directory you want to clean up or the text entry to write the path to your directory. Then click on Remove to clean the directory. The program can immediatly be ran again on a new directory.
+Depending on the command line you run, you can use a GUI or not.
+### Use GUI
+Open the GUI simply by running the command `python rm_empty.py` in a terminal. It will open a window where you can either type the path to the directory you want to clean up or click on the Browse directories button to select the directory. 
 ![UI window](./ui.png)
+To use the GUI, make sure you have the `tkinter` library properly installed.
+### Use terminal
+Run the command `python rm_empty.py --no-gui dir_to_path` (or simply `python rm_empty.py dir_to_path`) to clean the directory located at `dir_to_path`.<br>
+You can also run `python rm_empty --no-gui` and the program will ask you to enter the path to the directory you want to clean.
 
 ## To do
 The following things might be added in the future to improve this small script:
-- [ ] Check for invalid arguments
-- [ ] Use `sys` library to be able to run `python3 ./rm_empty.py path_to_dir` instead of changing the source code
+- [x] Check for invalid arguments
+- [x] Use `sys` library to be able to run `python3 ./rm_empty.py path_to_dir` instead of changing the source code
 - [x] Add a simple UI to select the directory and run the script
-- [ ] Add the possibility to use UI or terminal
+- [x] Add the possibility to use UI or terminal
+- [ ] Add option to remove empty files too
--- rm_empty.py
+++ rm_empty.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tkinter as tk
 import tkinter.filedialog as fd
 
@@ -16,7 +17,7 @@ def rm_empty(dir_path: str) -> int:
     rm_count = 0
     if not os.path.isdir(dir_path): # handles the case where directory path is wrong 
         print(f"ERROR: {dir_path} is not a directory.")
-        return rm_count
+        return -1
     ls = os.listdir(dir_path)
     for item in ls:
         item_path = os.path.join(dir_path, item)
@@ -36,31 +37,49 @@ def select_dir():
 def gui_rm_empty():
     dir = dir_entry.get()
     nb_dirs_rm = rm_empty(dir)
-    nb_dirs_rm_label.config(text=f"Removed {nb_dirs_rm} empty directories")
+    if nb_dirs_rm == -1: # invalid path
+        nb_dirs_rm_label.config(text=f"ERROR: {dir} is not a directory.")
+    else:
+        nb_dirs_rm_label.config(text=f"Removed {nb_dirs_rm} empty directories.")
     dir_entry.delete(0, 'end')
 
-# main window
-window = tk.Tk()
-window.title("rm empty")
 
-# label displaying the selected directory
-dir_label = tk.Label(window, text="Selected directory")
-dir_label.pack()
+if __name__ == "__main__":
+    if len(sys.argv) == 1: # open gui
+        # main window
+        window = tk.Tk()
+        window.title("rm empty")
 
-# entry to enter the path of the directory to cleanup
-dir_entry = tk.Entry(window, width=50)
-dir_entry.pack()
+        # label displaying the selected directory
+        dir_label = tk.Label(window, text="Selected directory")
+        dir_label.pack()
 
-# label showing the number of removed directories
-nb_dirs_rm_label = tk.Label(window, text="")
-nb_dirs_rm_label.pack()
+        # entry to enter the path of the directory to cleanup
+        dir_entry = tk.Entry(window, width=50)
+        dir_entry.pack()
 
-# button to open directory selection dialog
-select_bouton = tk.Button(window, text="Browse directories", command=select_dir)
-select_bouton.pack()
+        # label showing the number of removed directories
+        nb_dirs_rm_label = tk.Label(window, text="")
+        nb_dirs_rm_label.pack()
 
-# start button
-nettoyer_bouton = tk.Button(window, text="Remove", command=gui_rm_empty)
-nettoyer_bouton.pack()
+        # button to open directory selection dialog
+        select_bouton = tk.Button(window, text="Browse directories", command=select_dir)
+        select_bouton.pack()
 
-window.mainloop()
+        # start button
+        nettoyer_bouton = tk.Button(window, text="Remove", command=gui_rm_empty)
+        nettoyer_bouton.pack()
+
+        window.mainloop()
+    else:
+        args = sys.argv[1:] # get command ligne arguments
+        # parse argument
+        if len(args) == 1:
+            if args[0] == "--no-gui":
+                path_to_dir = input("Path to the directory you want to clean:   ")
+            else: 
+                path_to_dir = args[0]
+            rm_count = rm_empty(path_to_dir)
+            print(f"Removed {rm_count} empty directories.")
+        else:
+            print("Invalid command format.\nUse: python ./rm_empty.py [--no-gui] path_to_dir")
